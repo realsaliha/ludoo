877b53ae1d212efb17ead1f360b95bdac48fd27b
realsaliha <salihajaved001@gmail.com>
2026-03-31 19:43:02 +0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import Ludo from "./components/Ludo";
+import "./style.css";
+export default function App() {
+  return (
+    <>
+    <Ludo />
+    </>
+  );
+}
---
+++ src/components/Ludo.jsx
@@ -0,0 +1,163 @@
+export default function Ludo() {
+    return (
+        <>
+        <div class="board">
+  <div class="row">
+    <div class="red-box">
+      <div class="white-box">
+        <div class="red circle"></div>
+        <div class="red circle"></div>
+        <div class="red circle"></div>
+        <div class="red circle"></div>
+      </div>
+    </div>
+    <div class="cell-container">
+      <div class="row">
+        <div class="cell"></div>
+        <div class="cell"></div>
+        <div class="cell"></div>
+      </div>
+      <div class="row">
+        <div class="cell"></div>
+        <div class="cell green"></div>
+        <div class="cell green"></div>
+      </div>
+      <div class="row">
+        <div class="cell"></div>
+        <div class="cell green"></div>
+        <div class="cell"></div>
+      </div>
+      <div class="row">
+        <div class="cell"></div>
+        <div class="cell green"></div>
+        <div class="cell"></div>
+      </div>
+      <div class="row">
+        <div class="cell"></div>
+        <div class="cell green"></div>
+        <div class="cell"></div>
+      </div>
+      <div class="row">
+        <div class="cell"></div>
+        <div class="cell green"></div>
+        <div class="cell"></div>
+      </div>
+    </div>
+    <div class="green-box">
+      <div class="white-box">
+        <div class="green circle"></div>
+        <div class="green circle"></div>
+        <div class="green circle"></div>
+        <div class="green circle"></div>
+      </div>
+    </div>
+  </div>
+  <div class="row">
+    <div class="cell-container">
+      <div class="row">
+        <div class="cell"></div>
+        <div class="cell red"></div>
+        <div class="cell"></div>
+        <div class="cell"></div>
+        <div class="cell"></div>
+        <div class="cell"></div>
+      </div>
+      <div class="row">
+        <div class="cell"></div>
+        <div class="cell red"></div>
+        <div class="cell red"></div>
+        <div class="cell red"></div>
+        <div class="cell red"></div>
+        <div class="cell red"></div>
+      </div>
+      <div class="row">
+        <div class="cell"></div>
+        <div class="cell"></div>
+        <div class="cell"></div>
+        <div class="cell"></div>
+        <div class="cell"></div>
+        <div class="cell"></div>
+      </div>
+    </div>
+    <div class="destination"></div>
+    <div class="cell-container">
+      <div class="row">
+        <div class="cell"></div>
+        <div class="cell"></div>
+        <div class="cell"></div>
+        <div class="cell"></div>
+        <div class="cell"></div>
+        <div class="cell"></div>
+      </div>
+      <div class="row">
+        <div class="cell yellow"></div>
+        <div class="cell yellow"></div>
+        <div class="cell yellow"></div>
+        <div class="cell yellow"></div>
+        <div class="cell yellow"></div>
+        <div class="cell"></div>
+      </div>
+      <div class="row">
+        <div class="cell"></div>
+        <div class="cell"></div>
+        <div class="cell"></div>
+        <div class="cell"></div>
+        <div class="cell yellow"></div>
+        <div class="cell"></div>
+      </div>
+    </div>
+  </div>
+  <div class="row">
+    <div class="blue-box">
+      <div class="white-box">
+        <div class="blue circle"></div>
+        <div class="blue circle"></div>
+        <div class="blue circle"></div>
+        <div class="blue circle"></div>
+      </div>
+    </div>
+    <div class="cell-container">
+      <div class="row">
+        <div class="cell"></div>
+        <div class="cell blue"></div>
+        <div class="cell"></div>
+      </div>
+      <div class="row">
+        <div class="cell"></div>
+        <div class="cell blue"></div>
+        <div class="cell "></div>
+      </div>
+      <div class="row">
+        <div class="cell"></div>
+        <div class="cell blue"></div>
+        <div class="cell"></div>
+      </div>
+      <div class="row">
+        <div class="cell"></div>
+        <div class="cell blue"></div>
+        <div class="cell"></div>
+      </div>
+      <div class="row">
+        <div class="cell blue"></div>
+        <div class="cell blue"></div>
+        <div class="cell"></div>
+      </div>
+      <div class="row">
+        <div class="cell"></div>
+        <div class="cell"></div>
+        <div class="cell"></div>
+      </div>
+    </div>
+    <div class="yellow-box">
+      <div class="white-box">
+        <div class="yellow circle"></div>
+        <div class="yellow circle"></div>
+        <div class="yellow circle"></div>
+        <div class="yellow circle"></div>
+      </div>
+    </div>
+  </div>
+</div>
+        </>
+    );
+}
